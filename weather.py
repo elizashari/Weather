b85908937ea7192d15cac85e3df357a485d5075b
Elizaveta Shvedova <130368472+elizashari@users.noreply.github.com>
2023-04-27 11:27:56 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,24 +1,14 @@
 
 import requests
 
-def is_cyrrylic(symb):
-    if u'\u0400' <= symb <=u'\u04FF' or u'\u0500' <= symb <= u'\u052F':
-        return True
-    else:
-        return False
-
 
 def show_weather(places):
     for place in places:
-        if is_cyrrylic(place):
-            payload = {'nqTM': '', 'lang': 'ru'}
-        else:
-            payload = {'nqTM': '', 'lang': 'en'}
-        url = 'http://wttr.in/{}'.format(place)
+        payload = {'nqTM': '', 'lang': 'ru'}
         response = requests.get(url, params=payload)
         response.raise_for_status()
         print(response.text)
-
+        
 
 def main():
     places = ['London', 'SVO', 'Череповец']
